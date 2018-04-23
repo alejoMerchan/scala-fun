1fccd459e6d71ddc8e430c41d11946680a1e9009
amerchan <amercha@velocitypartners.com>
2018-04-23 17:42:58 -0500

    - reto alex.

--- src/test/scala/fun/examples/challenges/challenges.scala
+++ src/test/scala/fun/examples/challenges/challenges.scala
@@ -5,6 +5,9 @@ import org.scalatest.FunSuite
 
 class challenges extends FunSuite {
 
+  test("654"){
+    println(' '.isWhitespace)
+  }
 
   test("reduce by pairs") {
 
@@ -21,12 +24,53 @@ class challenges extends FunSuite {
 
     def reduceByPairs(s: String): String = {
       aux(aux(s)) match {
-        case s: String => if(s.isEmpty) "Empty String" else s
+        case s: String => if (s.isEmpty) "Empty String" else s
       }
     }
 
 
     println(reduceByPairs(""))
+  }
+
+  test("") {
+
+    def operation(a: Int, b: Int, operation: Char): Char = {
+      operation match {
+        case '+' => (a + b).toString.charAt(0)
+        case '-' => (a - b).toString.charAt(0)
+        case '*' => (a * b).toString.charAt(0)
+        case '/' => (a / b).toString.charAt(0)
+      }
+    }
+
+    def metodoAux(listNums: List[Char], signo: Char): List[Char] = {
+      listNums match {
+        case a :: rest =>
+          val y = (operation( rest.head.asDigit, a.asDigit , signo) :: rest.tail)
+          y
+      }
+    }
+    def operando(operacion:String):String  = {
+      val aux = List.empty[Char]
+      val y = (operacion foldLeft aux) {
+        (a, b) =>
+          if (b.isDigit) {
+            b :: a
+          } else {
+            if(!b.isWhitespace)
+            metodoAux(a, b)
+            else{
+              a
+            }
+          }
+      }
+
+      y.head.toString
+    }
+
+    println("resultado: " + operando("5 1 2 + 4 * + 3 -"))
+
+
   }
 
 }
